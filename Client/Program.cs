4a4dd865d3d01d9c02939018c1dcb19341244bcf
Emino_wp61@itstep.edu.az <Emino_wp61@itstep.edu.az>
2023-12-01 15:54:11 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+int screenWidth = 1920;
+int screenHeight = 1080;
+
+using (Bitmap bitmap = new Bitmap(screenWidth, screenHeight))
+{
+    using (Graphics graphics = Graphics.FromImage(bitmap))
+    {
+        graphics.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size(screenWidth, screenHeight));
+    }
+
+    string fileName = $"EkranGoruntusu_{DateTime.Now:yyyyMMddHHmmss}.png";
+    bitmap.Save(fileName);
+}
---
+++ Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+
+
+#pragma warning disable
+byte[] CaptureAndSaveScreenshot()
+{
+    int width = 1920;
+    int height = 1080;
+
+    using (Bitmap bmp = new Bitmap(width, height))
+    {
+        using (Graphics g = Graphics.FromImage(bmp))
+        {
+            g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+        }
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bmp.Save(stream, ImageFormat.Jpeg);
+            return stream.ToArray();
+        }
+    }
+}
+UdpClient udpClient = new UdpClient(27001);
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+while (true)
+{
+    //try
+    //{
+    var result = await udpClient.ReceiveAsync();
+    remoteEP = result.RemoteEndPoint;
+    new Task(async () =>
+    {
+        await Console.Out.WriteLineAsync(remoteEP.ToString());
+        while (true)
+        {
+            var screen = CaptureAndSaveScreenshot();
+            //var imgByte = ImageToByteArray(screen);
+            var chunks = screen.Chunk(ushort.MaxValue - 29);
+
+            foreach (var chunk in chunks)
+            {
+                await udpClient.SendAsync(chunk, chunk.Length, remoteEP);
+            }
+
+        }
+
+    }).Start();
+
+    //}
+    //catch (Exception)
+    //{
+    //    continue;
+
+    //}
+
+}
